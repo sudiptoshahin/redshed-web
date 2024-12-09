789aedffa751aa87da906e717cacc439d90af78b
sudiptoshahin <sudipto.shine@gmail.com>
2024-12-10 01:29:25 +0600

    working on type forms

--- backend/boardman/views.py
+++ backend/boardman/views.py
@@ -3,6 +3,31 @@ from django.shortcuts import render
 # from django.contrib.auth import authenticate, login, logout
 
 
+type_context_dict = {
+        "table_headers": ['Typeees', 'Status', 'Created At', 'Actions'],
+        "data": [
+            {
+                "id": "#001",
+                "title": "Formal", 
+                "status": True,
+                "created_at": "12-11-2024"
+            },
+            {
+                "id": "#002",
+                "title": "Casual", 
+                "status": False,
+                "created_at": "12-11-2024"
+            },
+            {
+                "id": "#003",
+                "title": "Sports", 
+                "status": False,
+                "created_at": "13-11-2024"
+            }
+        ]
+    }
+
+
 def admin_login(request):
     return render(request, 'boardman/login.html')
 
@@ -12,7 +37,12 @@ def admin_dashboard(request):
 
 
 def admin_inventory_type(request):
-    return render(request, 'boardman/inventory/type.html')
+
+    # data that needs to pass in context dictionary
+    return render(request, 'boardman/inventory/type.html', {
+        'data': type_context_dict["data"],
+        'headers': type_context_dict["table_headers"]
+    })
 
 
 def admin_inventory_category(request):
