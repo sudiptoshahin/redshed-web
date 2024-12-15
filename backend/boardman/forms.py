797ff7a9ee316b740218ada325f5dc3f0bc55237
sudiptoshahin <mdsshahin784@gmail.com>
2024-12-15 20:34:31 +0600

    working on add type

--- backend/boardman/forms.py
+++ backend/boardman/forms.py
@@ -9,38 +9,17 @@ class TypeForm(ModelForm):
         fields = ['title', 'image', 'category_id', 'status']
 
     labels = {
-        'title': '',
-        'image': '',
-        'category': '',
-        'status': False
+        'title': 'Title',
+        'image': 'Image',
+        'category': 'Select category (*)',
+        'status': 'Status'
     }
     
-    widgets = {
-        'title': forms.TextInput
-    }
-
-
-# from django import forms
-# from datetime import datetime
-
-
-# class TypeForm(forms.Form):
-#     title = forms.CharField(
-#         label="Title",
-#         max_length=100,
-#         widget=forms.TextInput(
-#             attrs={
-#                 'class': 'form-control',
-#             }
-#         ),
-#         required=True
-#     )
-#     status = forms.BooleanField(
-#         label="Status",
-#         required=True,
-#         initial=False
-#     )
-#     created_at = forms.DateTimeField(
-#         label='Created At',
-#         initial=datetime.now()
-#     )
+    # widgets = {
+    #     'title': forms.TextInput(
+    #         attrs={
+    #             'class': 'input-label',
+    #             'placeholder': 'Enter the title'
+    #         }
+    #     )
+    # }
--- backend/boardman/views.py
+++ backend/boardman/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 # from django.contrib.auth import urls
 # from django.contrib.auth import authenticate, login, logout
+from boardman.forms import TypeForm
 
 
 type_context_dict = {
@@ -50,4 +51,11 @@ def admin_inventory_category(request):
 
 
 def admin_inventory_type_add(request):
-    return render(request, 'boardman/inventory/add_type.html')
+
+    typeForm = TypeForm()
+    if request.method == "POST":
+        print('title:', request.POST.get('title'))
+
+    return render(request, 'boardman/inventory/add_type.html', {
+        "form": typeForm
+    })
