a0fed2bea180e5ed22a8ed7f055dbaba4c64845d
sudiptoshahin <mdsshahin784@gmail.com>
2025-02-23 16:03:30 +0600

    change admin header

--- backend/boardman/admin.py
+++ backend/boardman/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 from boardman.models import User
 from django.contrib.auth.models import Permission, Group
+# from django.utils.translation import ugettext_lazy
 
+
+admin.site.site_header = 'Redshed Admin'
+admin.site.index_title = 'All Settings and customization'
+# admin.site.site_title = 'HTML title from adminsitration'
 
 admin.site.register(User)
 admin.site.register(Permission)
